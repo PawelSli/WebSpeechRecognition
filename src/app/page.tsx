f5d02358c24666b1976c4894f8e0320341c52b1d
Paweł Śliwa <pawel.sliwa@medcoreproject.pl>
2024-09-21 00:08:08 +0200

    Usunięcie SpeechRecognition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import SpeechRecognition from "@/components/SpeechRecognition";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <SpeechRecognition/>
     </main>
   );
 }
