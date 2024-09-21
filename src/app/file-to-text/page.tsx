8b001cfef7e4ab8080ec091916a813228c18f61c
Paweł Śliwa <pawel.sliwa@medcoreproject.pl>
2024-09-21 13:24:42 +0200

    Removing unnecessary text

--- src/app/file-to-text/page.tsx
+++ src/app/file-to-text/page.tsx
@@ -2,7 +2,6 @@ export default function FileToText() {
     return (
         <div>
             <h1>File to Text</h1>
-            <p>This is the File to Text page.</p>
         </div>
     );
 }
--- src/app/live-to-text/page.tsx
+++ src/app/live-to-text/page.tsx
@@ -2,7 +2,6 @@ export default function LiveToText() {
     return (
         <div>
             <h1>Live to Text</h1>
-            <p>This is the Live to Text page.</p>
         </div>
     );
 }
